1e65dc63998c4de1a54a0f16102f880d8e8d75fe
dskliar@productsavvy.com <Vasilisacat02!>
2022-06-14 14:39:58 +0300

    first test Page Object

---
+++ pages/base_page.py
@@ -0,0 +1,9 @@
+import pytest
+
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+        
+    def open(self):
+        self.browser.get(self.url)
